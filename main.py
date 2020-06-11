974071e4dc7719e95d199cf7a8dc28d391026105
Humberto Gonzalez <humberto.gonzalezj@gmail.com>
2020-06-10 18:11:03 -0700

    adding assignment file

--- main.py
+++ main.py
@@ -13,8 +13,14 @@ if __name__ == "__main__":
     #
     # 1. Retrieve a list of "events" associated with the given user name
     # 2. Print out the time stamp associated with the first event in that list.
+    
+    username = sys.argv[1]
 
-    print("COMPLETE THE TODOs")
+    response = requests.get("https://api.github.com/users/{}/events".format(username))
+    events = json.loads(response.content)
+    res = events[0]['created_at']
+
+    print(res)
     
 
 
